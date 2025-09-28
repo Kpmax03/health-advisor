ae76c3a5166e6940d185b953d87926f583d41ee4
kpmax03 <kp5911800@gmail.com>
2025-09-28 07:44:22 +0530

    implemented user entity with configuration

---
+++ src/main/java/com/health/advisor/entity/User.java
@@ -0,0 +1,24 @@
+package com.health.advisor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private String id;
+    @Column(unique = true)
+    private String userName;
+    private String password;
+
+}
