0ab947612de2de43f13b6026a1d5c03479ee5636
kpmax03 <kp5911800@gmail.com>
2025-10-02 01:09:53 +0530

    added external air apis service

---
+++ src/main/java/com/health/advisor/externalApis/AirConditionApis.java
@@ -0,0 +1,31 @@
+package com.health.advisor.externalApis;
+
+import com.health.advisor.RequestDto.AirQualityRequest;
+import com.health.advisor.config.AirQualityMapper;
+import com.health.advisor.entity.AirQuality;
+import com.health.advisor.repository.AirQualityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AirConditionApis {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private AirQualityRepository airQualityRepository;
+
+    @Scheduled(fixedRate = 60000)
+    public AirQuality getAirQuality(String city){
+        String API_URL="https://api.waqi.info/feed/"+city+"/?token="+System.getenv("AQICN_API_KEY");
+
+        AirQualityRequest template = restTemplate.getForObject(API_URL, AirQualityRequest.class);
+        System.out.println(template);
+        AirQuality airQualityEntity = AirQualityMapper.toEntity(template);
+        System.out.println(airQualityEntity);
+        AirQuality save = airQualityRepository.save(airQualityEntity);
+
+        return save;
+    }
+}
